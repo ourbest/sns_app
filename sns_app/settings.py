25041ce4e74083a428f7a51da3575fddd5e04495
ourbest <chenyonghui@gmail.com>
2017-12-19 17:16:54 +0800

    add 二手

--- sns_app/settings.py
+++ sns_app/settings.py
@@ -146,7 +146,7 @@ FILE_UPLOAD_HANDLERS = ["django.core.files.uploadhandler.TemporaryFileUploadHand
 EMAIL_HOST = 'smtp.mxhichina.com'
 EMAIL_HOST_USER = 'yonghui.chen@cutt.com'
 
-JS_VER = '461f8fdffda05aecde3a'
+JS_VER = '6161ba2a331a070cd7a3'
 QINIU_URL = 'http://jwres.cutt.com/'
 
 DEFAULT_CHARSET = 'utf-8'
