62a2a43a73f7db9e0692f05551ff4926ad810a3d
ourbest <chenyonghui@gmail.com>
2018-03-04 14:46:08 +0800

    plit

--- backend/apis.py
+++ backend/apis.py
@@ -1049,6 +1049,8 @@ def import_qun(app, ids, request, email, phone, edit_method, i_ignore_dup):
 
         to_delete.delete()
 
+    split_to_self = login_user.snsuser_set.filter(app=the_app, friend=1).count() if login_user else 0
+
     for line in ids.split('\n'):
         line = line.strip()
         if line:
@@ -1089,7 +1091,7 @@ def import_qun(app, ids, request, email, phone, edit_method, i_ignore_dup):
                         db.status = 2
                         db.snsgroupsplit_set.filter(status=0).update(status=3)
                         db.save()
-                elif login_user and (device or (the_app and the_app.self_qun == 1)) and login_user.app == the_app:
+                elif split_to_self and login_user and (device or (the_app and the_app.self_qun == 1)) and login_user.app == the_app:
                     SnsGroupSplit(group=db, user=login_user, phone=device).save()
             except:
                 logger.warning("error save %s" % line, exc_info=1)
@@ -1097,7 +1099,7 @@ def import_qun(app, ids, request, email, phone, edit_method, i_ignore_dup):
     logger.info('共%s个新群' % cnt)
 
     if not device:
-        if the_app and the_app.self_qun == 1 and login_user.app == the_app:
+        if split_to_self and the_app and the_app.self_qun == 1 and login_user.app == the_app:
             split_qun_to_device(request, email)
 
     return {
