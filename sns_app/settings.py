efd0525158bc4b863f536bd4a833f9f34dbe4563
ourbest <chenyonghui@gmail.com>
2018-04-08 11:14:10 +0800

    js

--- sns_app/settings.py
+++ sns_app/settings.py
@@ -168,7 +168,7 @@ FILE_UPLOAD_HANDLERS = ["django.core.files.uploadhandler.TemporaryFileUploadHand
 EMAIL_HOST = 'smtp.mxhichina.com'
 EMAIL_HOST_USER = 'yonghui.chen@cutt.com'
 
-JS_VER = 'fc12ed29b0b0cb990c25'
+JS_VER = '58f8308707639005d4e7'
 QINIU_URL = 'http://jwres.cutt.com/'
 
 DEFAULT_CHARSET = 'utf-8'
