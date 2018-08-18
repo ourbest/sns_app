180ce9344e5cb96de6063785d202f26ba19b1f70
ourbest <chenyonghui@gmail.com>
2018-08-18 23:35:35 +0800

    use pool

--- backend/management/commands/consumer.py
+++ backend/management/commands/consumer.py
@@ -7,27 +7,33 @@ from backend.loggs import logger
 
 
 class Command(BaseCommand):
+    def __init__(self):
+        super().__init__(stdout=None, stderr=None, no_color=False)
+        self.total_records = 0
+
     def handle(self, *args, **options):
+
         while True:
             self._consume()
 
     def _consume(self):
+        consumer = KafkaConsumer(settings.WEIZHAN_LOG_TOPIC, group_id='tuiguang',
+                                 bootstrap_servers=settings.BOOTSTRAP_SERVERS)
         try:
-            consumer = KafkaConsumer(settings.WEIZHAN_LOG_TOPIC, group_id='tuiguang',
-                                     bootstrap_servers=settings.BOOTSTRAP_SERVERS)
-
             while True:
                 self.open_consumer(consumer)
         except Exception as e:
             logger.error('Consumer error', exc_info=True)
-            if consumer:
-                consumer.close()
+            consumer.close()
 
     def open_consumer(self, consumer):
-        try:
-            for msg in consumer:
-                line = msg.value.decode('utf-8')
-                process_line(line)
-        except Exception as e:
-            self.stderr.write('error process {}'.format(e))
-            logger.error('Consumer error', exc_info=True)
+        messages = consumer.poll(500)
+        if messages:
+            for topic_messages in messages.values():
+                for message in topic_messages:
+                    line = message.value.decode('utf-8')
+                    try:
+                        process_line(line)
+                        self.total_records += 1
+                    except Exception:
+                        logger.error('Error process line %s' % line, exc_info=True)
